75b4c6c02516d61a75d07d667657ff84db3ed99f
zyc <zhangyic163@163.com>
2023-05-30 18:58:02 +0800

    添加Util类，类中添加求和以及求差的方法

---
+++ src/main/java/com/atguitu/git/test/UtilTest.java
@@ -0,0 +1,17 @@
+package com.atguitu.git.test;
+
+/**
+ * @author zyc
+ * @create 2023-05-30 18:56
+ */
+public class UtilTest {
+
+    public int sum(int a,int b){
+        return a+b;
+    }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
+
+}
