379ff00f2c55fbd5ccc5ccf286f2e18b39418df4
zyc <zhangyic163@163.com>
2023-05-30 18:55:01 +0800

    添加一个 main 方法

---
+++ src/main/java/com/atguitu/git/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguitu.git.test;
+
+/**
+ * @author zyc
+ * @create 2023-05-30 18:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
