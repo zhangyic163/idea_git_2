de3e9076f93ac2d9fca478eeaa9cfcfd38c4a0e4
zyc <zhangyic163@163.com>
2023-05-30 19:02:40 +0800

    Util类中随便写了个方法,回退版本后在原来方法的位置写了个方法

---
+++ src/main/java/com/atguitu/git/test/UtilTest.java
@@ -0,0 +1,26 @@
+package com.atguitu.git.test;
+
+/**
+ * @author zyc
+ * @create 2023-05-30 18:56
+ */
+public class UtilTest {
+
+    public int sum(int a,int b){
+        return a+b;
+    }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
+
+
+    public void bb (){
+        System.out.println("asdasfdfsfvbgfgfsd");
+    }
+
+    public void aa (){
+        System.out.println("asdasfdfsfvbgfgfsd");
+    }
+
+}
